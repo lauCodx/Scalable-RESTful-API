7a31f584c174940456314d582023eb330431ccbe
lauCodx <lauphix1@gmail.com>
2024-10-30 21:57:28 +0100

    Feat: Created the databse config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      cache:true,
+      load:[config]
+    }),
+
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async (config) =>({
+        uri:config.get('database.connectionString')
+      }),
+      inject:[ConfigService]
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
