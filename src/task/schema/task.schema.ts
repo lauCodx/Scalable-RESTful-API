04759b20c02dd7ca10520aa2199f4e6fcb319c90
lauCodx <lauphix1@gmail.com>
2024-11-06 23:46:24 +0100

    feat: updated my task serivce

--- src/task/schema/task.schema.ts
+++ src/task/schema/task.schema.ts
@@ -3,7 +3,7 @@ import mongoose, { Document, Schema as MongooseSchema } from "mongoose";
 
 @Schema({timestamps:true})
 export class Task extends Document {
-    @Prop({required: true, trim:true, lowercase:true})
+    @Prop({required: true, trim:true})
     title: string;
 
     @Prop({ required: true })
